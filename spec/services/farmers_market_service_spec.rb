175e0661903a805bb5d5a0e7c24f86cbdc906a0c
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-25 12:54:55 -0400

    test: update service tests for vendors for a market

--- spec/services/farmers_market_service_spec.rb
+++ spec/services/farmers_market_service_spec.rb
@@ -49,5 +49,29 @@ RSpec.describe 'Farmers Market Service' do
       expect(attributes).to have_key(:vendor_count)
       expect(attributes[:vendor_count]).to be_an(Integer)
     end
+
+    it 'can retrieve a list of vendors for a given market' do
+      vendors = FarmersMarketService.new.get_vendors_for_market(322458)
+
+      expect(vendors).to be_a(Hash)
+      expect(vendors[:data]).to be_an(Array)
+
+      vendor = vendors[:data].first
+
+      expect(vendor).to have_key(:id)
+      expect(vendor).to have_key(:type)
+      expect(vendor).to have_key(:attributes)
+
+      attributes = vendor[:attributes]
+
+      keys = [:name, :description, :contact_name, :contact_phone]
+      keys.each do |key|
+        expect(attributes).to have_key(key)
+        expect(attributes[key]).to be_a(String)
+      end
+
+      expect(attributes).to have_key(:credit_accepted)
+      expect(attributes[:credit_accepted]).to be_in([true, false])
+    end
   end
 end
