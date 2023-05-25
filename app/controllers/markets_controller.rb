546fc2ac92cebbd6fc4843da2993b49c0031d44a
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-25 12:56:05 -0400

    feat: update show action for markets controller

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,9 +1,10 @@
 class MarketsController < ApplicationController
   def index
-    @markets = MarketsFacade.new.get_markets
+    @markets = FarmersMarketFacade.new.get_markets
   end
 
   def show
-    @market = MarketsFacade.new.get_market(params[:id])
+    @market = FarmersMarketFacade.new.get_market(params[:id])
+    @vendors = FarmersMarketFacade.new.get_vendors_for_market(params[:id])
   end
 end
