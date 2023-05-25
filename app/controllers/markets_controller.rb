5984da831a424be2102e7e2a90901ba3cb8d718f
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-24 23:22:55 -0400

    feat: set up markets controller with index and show methods

---
+++ app/controllers/markets_controller.rb
@@ -0,0 +1,9 @@
+class MarketsController < ApplicationController
+  def index
+    @markets = MarketsFacade.get_markets
+  end
+
+  def show
+    @market = MarketsFacade.get_market_details(params[:id])
+  end
+end
